d23b2ac4240ef82657197ed715f16c1cd77da15a
Andrea Grillo <andrea.grillo@outlook.com>
2020-07-03 21:17:28 +0100

    [Web] Setup Swagger

--- src/CriThink.Server/CriThink.Web/Startup.cs
+++ src/CriThink.Server/CriThink.Web/Startup.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
 using CriThink.Common.Endpoints;
+using CriThink.Web.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,6 +12,7 @@ using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 // ReSharper disable UnusedMember.Global
 
@@ -31,9 +36,15 @@ namespace CriThink.Web
 
             // API versioning
             SetupAPIVersioning(services);
+
+            // Swagger
+            SetupSwagger(services);
+
+            services
+                .AddMvc(options => { options.EnableEndpointRouting = false; });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Called by framework")]
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -41,6 +52,13 @@ namespace CriThink.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(); // Swagger
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                options.DisplayRequestDuration();
+            });
 
             app.UseResponseCaching();
 
@@ -55,6 +73,8 @@ namespace CriThink.Web
                     await context.Response.WriteAsync("Hello World!").ConfigureAwait(false);
                 });
             });
+
+            app.UseMvc(); // Swagger
         }
 
         private static void SetupCache(IServiceCollection services)
@@ -71,6 +91,31 @@ namespace CriThink.Web
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 config.AssumeDefaultVersionWhenUnspecified = true;
                 config.ApiVersionReader = new HeaderApiVersionReader(EndpointConstants.ApiVersionHeader);
+            });
+        }
+
+        private void SetupSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<AddRequiredHeaderParameter>();
+
+                var contact = new OpenApiContact
+                {
+                    Name = Configuration["swaggerapiinfo:name"],
+                    Email = Configuration["swaggerapiinfo:email"],
+                    Url = new Uri(Configuration["SwaggerApiInfo:Uri"])
+                };
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = $"{Configuration["SwaggerApiInfo:Title"]} v1", Version = "v1", Contact = contact });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+
+                // Uses full schema names to avoid v1/v2/v3 schema collisions
+                // see: https://github.com/domaindrivendev/Swashbuckle/issues/442
+                //options.CustomSchemaIds(x => x.FullName);
             });
         }
     }
