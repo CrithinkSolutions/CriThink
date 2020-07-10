a8a9751693999954f58e36db512a99f46814509e
Andrea Grillo <andrea.grillo@outlook.com>
2020-07-10 23:47:36 +0100

    [Web/DomainAnlyzer/Endpoints] Add infrastructure to analyze the given URL with different services. This adds HTTPS analysis support

---
+++ src/CriThink.Server/CriThink.Server.Providers/CriThink.Server.Providers.DomainAnalyzer/Providers/BaseAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace CriThink.Server.Providers.DomainAnalyzer.Providers
+{
+    internal abstract class BaseAnalyzer : IAnalyzer
+    {
+        protected readonly ConcurrentQueue<Task<AnalysisResponse>> Queue;
+
+        private IAnalyzer _nextAnalyzer;
+
+        protected BaseAnalyzer(Uri uri, ConcurrentQueue<Task<AnalysisResponse>> queue)
+        {
+            Uri = uri;
+            Queue = queue;
+        }
+
+        protected Uri Uri { get; }
+
+        public IAnalyzer SetNext(IAnalyzer analyzer)
+        {
+            _nextAnalyzer = analyzer;
+            return _nextAnalyzer;
+        }
+
+        public virtual Task<AnalysisResponse>[] AnalyzeAsync()
+        {
+            var nextAnalyzer = _nextAnalyzer?.AnalyzeAsync();
+            return nextAnalyzer ?? Queue.ToArray();
+        }
+    }
+
+    internal interface IAnalyzer
+    {
+        /// <summary>
+        /// Add the next <see cref="IAnalyzer"/> to the chain
+        /// </summary>
+        /// <param name="analyzer"><see cref="IAnalyzer"/> instance</param>
+        /// <returns>Returns the given <see cref="IAnalyzer"/> instance</returns>
+        IAnalyzer SetNext(IAnalyzer analyzer);
+
+        /// <summary>
+        /// Start the analyzer
+        /// </summary>
+        /// <returns>Returns the list of analysis responses</returns>
+        Task<AnalysisResponse>[] AnalyzeAsync();
+    }
+}
--- src/CriThink.Server/CriThink.Server.Web/Services/DomainAnalyzerService.cs
+++ src/CriThink.Server/CriThink.Server.Web/Services/DomainAnalyzerService.cs
@@ -1,19 +1,61 @@
 ﻿using System;
-using CriThink.Server.Web.Models.DTOs.NewsAnalyzer.Responses;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CriThink.Common.Endpoints.DTOs.NewsAnalyzer;
+using CriThink.Server.Providers.DomainAnalyzer;
+using CriThink.Server.Web.Facades;
 
 namespace CriThink.Server.Web.Services
 {
     public class DomainAnalyzerService : IDomainAnalyzerService
     {
-        public HttpsSupportResponse HasUriHttpsSupport(Uri uri)
+        private readonly IDomainAnalyzerFacade _domainAnalyzerFacade;
+        private readonly IMapper _mapper;
+
+        public DomainAnalyzerService(IDomainAnalyzerFacade domainAnalyzerProvider, IMapper mapper)
+        {
+            _domainAnalyzerFacade = domainAnalyzerProvider ?? throw new ArgumentNullException(nameof(domainAnalyzerProvider));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<NewsAnalyzerResponse> HasUriHttpsSupportAsync(Uri uri)
         {
             if (uri == null)
                 throw new ArgumentNullException(nameof(uri));
 
-            return new HttpsSupportResponse
-            {
-                IsSecure = uri.Scheme == Uri.UriSchemeHttps
-            };
+            var analysisResponse = await _domainAnalyzerFacade.HasHttpsSupportAsync(uri).ConfigureAwait(false);
+            if (analysisResponse.HasError)
+                throw analysisResponse.Exception;
+
+            var response = _mapper.Map<AnalysisResponse, NewsAnalyzerResponse>(analysisResponse);
+            return response;
+        }
+
+        public async Task<NewsAnalyzerResponse> AnalyzeDomainAsync(Uri uri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+
+            var analysisResponse = await _domainAnalyzerFacade.GetDomainInfoAsync(uri).ConfigureAwait(false);
+
+            if (analysisResponse.HasError)
+                throw analysisResponse.Exception;
+
+            var response = _mapper.Map<AnalysisResponse, NewsAnalyzerResponse>(analysisResponse);
+            return response;
+        }
+
+        public async Task<NewsAnalyzerResponse[]> GetCompleteAnalysisAsync(Uri uri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+
+            var analysisResponses = await _domainAnalyzerFacade.GetCompleteAnalysisAsync(uri).ConfigureAwait(false);
+
+            return analysisResponses
+                .Select(r => _mapper.Map<AnalysisResponse, NewsAnalyzerResponse>(r))
+                .ToArray();
         }
     }
 
@@ -26,7 +68,21 @@ namespace CriThink.Server.Web.Services
         /// Returns a status to identify if the provided URI has HTTPS support or not
         /// </summary>
         /// <param name="uri"><see cref="Uri"/> to analyze</param>
-        /// <returns>A response indicating the result</returns>
-        HttpsSupportResponse HasUriHttpsSupport(Uri uri);
+        /// <returns>A response indicating the analysis result</returns>
+        Task<NewsAnalyzerResponse> HasUriHttpsSupportAsync(Uri uri);
+
+        /// <summary>
+        /// Analyze the domain of the given <see cref="Uri"/>
+        /// </summary>
+        /// <param name="uri"><see cref="Uri"/> to analyze</param>
+        /// <returns>A response indicating the analysis result</returns>
+        Task<NewsAnalyzerResponse> AnalyzeDomainAsync(Uri uri);
+
+        /// <summary>
+        /// Perform an analysis of the given <see cref="Uri"/> using all the NewsAnalyzer available
+        /// </summary>
+        /// <param name="uri"><see cref="Uri"/> to analyze</param>
+        /// <returns>A response indicating the analysis result</returns>
+        Task<NewsAnalyzerResponse[]> GetCompleteAnalysisAsync(Uri uri);
     }
 }
