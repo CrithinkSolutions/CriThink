88601487b324a1f492289c991ccd69ce0532894d
Andrea Grillo <andrea.grillo@outlook.com>
2020-08-27 00:24:03 +0100

    [Infrastructure/Core] Tweak to DB schema

--- src/CriThink.Server/CriThink.Server.Core/Entities/Question.cs
+++ src/CriThink.Server/CriThink.Server.Core/Entities/Question.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MediatR;
 
 namespace CriThink.Server.Core.Entities
 {
-    public class Question : ICriThinkIdentity
+    public class Question : ICriThinkIdentity, IRequest<Unit>
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
+        [Required]
         public string Text { get; set; }
     }
 }
---
+++ src/CriThink.Server/CriThink.Server.Infrastructure/Migrations/20200826232308_DemoQuestionsTweaks.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#pragma warning disable CA1062 // Validate arguments of public methods
+namespace CriThink.Server.Infrastructure.Migrations
+{
+    public partial class DemoQuestionsTweaks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionAnswers_DemoNews_DemoNewsId",
+                table: "QuestionAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionAnswers_Questions_QuestionId",
+                table: "QuestionAnswers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Questions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "QuestionId",
+                table: "QuestionAnswers",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DemoNewsId",
+                table: "QuestionAnswers",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "DemoNews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "DemoNews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionAnswers_DemoNews_DemoNewsId",
+                table: "QuestionAnswers",
+                column: "DemoNewsId",
+                principalTable: "DemoNews",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionAnswers_Questions_QuestionId",
+                table: "QuestionAnswers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionAnswers_DemoNews_DemoNewsId",
+                table: "QuestionAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionAnswers_Questions_QuestionId",
+                table: "QuestionAnswers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "QuestionId",
+                table: "QuestionAnswers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DemoNewsId",
+                table: "QuestionAnswers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "DemoNews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "DemoNews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionAnswers_DemoNews_DemoNewsId",
+                table: "QuestionAnswers",
+                column: "DemoNewsId",
+                principalTable: "DemoNews",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionAnswers_Questions_QuestionId",
+                table: "QuestionAnswers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
