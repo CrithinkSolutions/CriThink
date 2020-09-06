c5bcd61d2da5624ed8ea1406aef27db32c7571cb
GabAlpha <gabriele.alfarano@gmail.com>
2020-09-06 19:00:22 +0200

    [Web] Fixed 404 route from url

--- src/CriThink.Server/CriThink.Server.Web/ClientApp/src/App.jsx
+++ src/CriThink.Server/CriThink.Server.Web/ClientApp/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Redirect } from 'react-router'
+import { Route, Redirect, Switch } from 'react-router'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Layout } from './components/Layout'
@@ -27,18 +27,20 @@ class App extends Component {
 
   render () {
     return (
-      <Layout>
-      	<Route exact path='/' component={Home} />
-        <NoAuthRoute exact path='/login' component={LoginArea} />
-        <AuthRoute authed={this.props.jwtToken} exact path='/signup' component={SignUpArea} />
-        <AuthRoute authed={this.props.jwtToken} exact path='/forgotpassword' component={ForgotPwdArea} />
-        <AuthRoute authed={this.props.jwtToken} exact path='/profile' component={ProfileArea} />
-        <AuthRoute authed={this.props.jwtToken} exact path='/profile/changepassword' component={ChangePwdArea} />
-        <AuthRoute authed={this.props.jwtToken} path='/api/identity/reset-password' component={NewPwdArea} />
-        <AuthRoute authed={this.props.jwtToken} path='/backoffice' component={Backoffice} />
-        {this.props.dialog}
-        <Redirect from='*' to='/'/>
-      </Layout>
+        <Layout>
+          <Switch>
+          	<Route exact path='/' component={Home} />
+            <NoAuthRoute exact path='/login' component={LoginArea} />
+            <AuthRoute authed={this.props.jwtToken} exact path='/signup' component={SignUpArea} />
+            <AuthRoute authed={this.props.jwtToken} exact path='/forgotpassword' component={ForgotPwdArea} />
+            <AuthRoute authed={this.props.jwtToken} exact path='/profile' component={ProfileArea} />
+            <AuthRoute authed={this.props.jwtToken} exact path='/profile/changepassword' component={ChangePwdArea} />
+            <AuthRoute authed={this.props.jwtToken} path='/api/identity/reset-password' component={NewPwdArea} />
+            <AuthRoute authed={this.props.jwtToken} path='/backoffice' component={Backoffice} />
+            {this.props.dialog}
+            <Redirect to='/' />
+          </Switch>
+        </Layout>
     );
   }
 }
