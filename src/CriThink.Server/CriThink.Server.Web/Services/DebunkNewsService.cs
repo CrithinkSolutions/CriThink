25fed0c2ac9dce9c560f7c89241b772c982864b9
Andrea Grillo <andrea.grillo@outlook.com>
2020-09-10 22:35:23 +0100

    [Web] Scrape news from OpenOnline

--- src/CriThink.Server/CriThink.Server.Web/Services/DebunkNewsService.cs
+++ src/CriThink.Server/CriThink.Server.Web/Services/DebunkNewsService.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using CriThink.Server.Providers.NewsAnalyzer;
+using CriThink.Server.Providers.NewsAnalyzer.Managers;
 using CriThink.Server.Web.Facades;
 
 namespace CriThink.Server.Web.Services
@@ -7,15 +10,33 @@ namespace CriThink.Server.Web.Services
     public class DebunkNewsService : IDebunkNewsService
     {
         private readonly IDebunkNewsFetcherFacade _debunkNewsFetcherFacade;
+        private readonly INewsScraperManager _newsScraperManager;
 
-        public DebunkNewsService(IDebunkNewsFetcherFacade debunkNewsFetcherFacade)
+        public DebunkNewsService(IDebunkNewsFetcherFacade debunkNewsFetcherFacade, INewsScraperManager newsScraperManager)
         {
             _debunkNewsFetcherFacade = debunkNewsFetcherFacade ?? throw new ArgumentNullException(nameof(debunkNewsFetcherFacade));
+            _newsScraperManager = newsScraperManager ?? throw new ArgumentNullException(nameof(newsScraperManager));
         }
 
         public async Task UpdateRepositoryAsync()
         {
-            await _debunkNewsFetcherFacade.FetchOpenOnlineDebunkNewsAsync().ConfigureAwait(false);
+            var debunkingNewsCollection = await _debunkNewsFetcherFacade.FetchOpenOnlineDebunkNewsAsync().ConfigureAwait(false);
+
+            var scrapedNewsCollection = new List<NewsScraperProviderResponse>();
+
+            foreach (var debunkingNews in debunkingNewsCollection)
+            {
+                if (debunkingNews.HasError)
+                    throw debunkingNews.Exception;
+
+                foreach (var response in debunkingNews.Responses)
+                {
+                    var scrapedNews = await _newsScraperManager.ScrapeNewsWebPage(new Uri(response.Link)).ConfigureAwait(false);
+                    scrapedNewsCollection.Add(scrapedNews);
+                }
+            }
+
+            // TODO: Add in db
         }
     }
 
