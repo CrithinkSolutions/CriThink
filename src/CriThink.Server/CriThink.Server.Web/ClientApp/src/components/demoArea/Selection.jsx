24262f742d7c2e3ef5b87e56e88c7d61690d0baa
GabAlpha <gabriele.alfarano@gmail.com>
2020-09-12 18:39:59 +0200

    [Web] Added white bg icon and more

--- src/CriThink.Server/CriThink.Server.Web/ClientApp/src/components/demoArea/Selection.jsx
+++ src/CriThink.Server/CriThink.Server.Web/ClientApp/src/components/demoArea/Selection.jsx
@@ -5,7 +5,7 @@ import { Card, Image, Icon, Popup } from "semantic-ui-react";
 import Book from "./../../svg/bookreader.svg";
 import Check from "./../../svg/check.svg";
 import Graduation from "./../../svg/graduation.svg";
-import LogoRound from "./../../svg/logoround.svg";
+import LogoRound from "./../../svg/logoroundw.svg";
 
 export class SelectionArea extends Component {
   render() {
@@ -15,72 +15,78 @@ export class SelectionArea extends Component {
           <Card.Group itemsPerRow={4} className="menucard">
             <Card>
               <Card.Content>
-                <p className="regular">Check Your News</p>
+                <p className="regular menutitle">Check Your News</p>
                 <Image src={Book} />
-                <Card.Content className="txtRight">
-                  <Link to="/3">
-                    <Popup
-                      content="Check your news"
+                <p className='light p-4'>Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </Card.Content>
+              <Link to="/3" className='txtRight p-3'>
+                <Popup
+                  content="Check your news"
+                  trigger={
+                    <span className="whitelink bold">
+                      CHECK <Icon name="long arrow alternate right" />
+                    </span>
+                  }
+                ></Popup>
+              </Link>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <p className="regular menutitle">Debunked Fake News</p>
+                <Image src={Check} style={{ height: "59px" }} />
+                <p className='light p-4'>Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </Card.Content>
+              <div className='txtRight p-3'>
+                <Popup
+                      content="Section under construction..."
                       trigger={
-                        <span className="whitelink bold">
-                          CHECK <Icon name="long arrow alternate right" />
+                        <span className="nolink bold">
+                          DEBUNK <Icon name="long arrow alternate right" />
                         </span>
                       }
-                    ></Popup>
-                  </Link>
-                </Card.Content>
-              </Card.Content>
+                ></Popup>
+              </div>
             </Card>
 
             <Card>
               <Card.Content>
-                <p className="regular">Debunked Fake News</p>
-                <Image src={Check} style={{ height: "59px" }} />
-                <Card.Content className="txtRight">
-                  <Popup
-                    content="Section under construction..."
-                    trigger={
-                      <span className="nolink bold">
-                        DEBUNK <Icon name="long arrow alternate right" />
-                      </span>
-                    }
-                  ></Popup>
-                </Card.Content>
-              </Card.Content>
-            </Card>
-
-            <Card>
-              <Card.Content>
-                <p className="regular">Spot Fake News</p>
+                <p className="regular menutitle">Spot Fake News</p>
                 <Image src={Graduation} />
-                <Card.Content className="txtRight">
-                  <Popup
-                    content="Section under construction..."
-                    trigger={
-                      <span className="nolink bold">
-                        LEARN <Icon name="long arrow alternate right" />
-                      </span>
-                    }
-                  ></Popup>
-                </Card.Content>
+                <p className='light p-4'>Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </Card.Content>
+              <div className='txtRight p-3'>
+                <Popup
+                      content="Section under construction..."
+                      trigger={
+                        <span className="nolink bold">
+                          LEARN <Icon name="long arrow alternate right" />
+                        </span>
+                      }
+                ></Popup>
+              </div>
             </Card>
 
             <Card>
               <Card.Content>
-                <p className="regular">Quiz & Games</p>
+                <p className="regular menutitle">Quiz & Games</p>
                 <Image src={LogoRound} />
-                <Card.Content className="txtRight">
-                  <Popup
-                    content="Section under construction..."
-                    trigger={
-                      <span className="nolink bold">
-                        PLAY <Icon name="long arrow alternate right" />
-                      </span>
-                    }
-                  ></Popup>
-                </Card.Content>
+                <p className='light p-4'>Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </Card.Content>
+              <div className='txtRight p-3'>
+                <Popup
+                      content="Section under construction..."
+                      trigger={
+                        <span className="nolink bold">
+                          PLAY <Icon name="long arrow alternate right" />
+                        </span>
+                      }
+                ></Popup>
+              </div>
             </Card>
           </Card.Group>
         </WaveDown>
