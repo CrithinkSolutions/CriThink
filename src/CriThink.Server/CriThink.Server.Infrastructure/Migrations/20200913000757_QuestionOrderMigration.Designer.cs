9f0e8e0be637e465d79f38a9fca785f33651d9ba
Andrea Grillo <andrea.grillo@outlook.com>
2020-09-13 01:16:29 +0100

    [Web/Infrastructure/Core/Endpoints] Add column Order to Questions table + API update

--- src/CriThink.Server/CriThink.Server.Infrastructure/Migrations/20200913000757_QuestionOrderMigration.Designer.cs
+++ src/CriThink.Server/CriThink.Server.Infrastructure/Migrations/20200913000757_QuestionOrderMigration.Designer.cs
@@ -4,14 +4,16 @@ using CriThink.Server.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CriThink.Server.Infrastructure.Migrations
 {
     [DbContext(typeof(CriThinkDbContext))]
-    partial class CriThinkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200913000757_QuestionOrderMigration")]
+    partial class QuestionOrderMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
