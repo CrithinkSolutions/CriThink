e0977aed6b6261b18ac50dfce16add6b8fa58c9c
GabAlpha <gabriele.alfarano@gmail.com>
2020-10-12 22:42:50 +0200

    [Backoffice] Login action

---
+++ src/CriThink.Server/CriThink.Server.Web/wwwroot/js/fetch.js
@@ -0,0 +1,34 @@
+function loginAPI(username,email,password,errorEl,clickEl) {
+	$('#'+clickEl).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
+	fetch('/api/identity/login', {
+		method: 'POST',
+		headers: {
+    		'Content-Type': 'application/json',
+  		},
+  		body: JSON.stringify({
+  			'email': email,
+  			'username': username,
+  			'password': password
+  		}),
+	})
+	.then(response => response.json())
+	.then(data => {
+		if(data.statusCode == 200) {
+			console.log('Success:', data);
+			$(errorEl).addClass('d-none');
+		}
+  		else {
+  			$(errorEl).removeClass('d-none');
+  		}
+	})
+	.then(() => {
+		$('#'+clickEl).html('Login');
+	});
+}
+
+$("#btn-login").click(function(event) {
+   	event.preventDefault();
+   	const username = $('#inputemail').val();
+   	const password = $('#inputpwd').val();
+   	username.includes('@')? loginAPI('',username,password,'#errordiv', event.target.id) : loginAPI(username,'',password,'#errordiv', event.target.id);
+});
