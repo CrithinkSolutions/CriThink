b2707351b4aaf89018b4185199bfcdab57f14c65
Andrea Grillo <andrea.grillo@outlook.com>
2020-10-17 17:29:50 +0100

    [Droid] Creates MainApplication.cs

--- src/CriThink.Client/CriThink.Client.Droid/SplashScreen.cs
+++ src/CriThink.Client/CriThink.Client.Droid/SplashScreen.cs
@@ -1,6 +1,5 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using MvvmCross.Platforms.Android.Core;
 using MvvmCross.Platforms.Android.Views;
 
 namespace CriThink.Client.Droid
@@ -10,7 +9,7 @@ namespace CriThink.Client.Droid
         , MainLauncher = true
         , NoHistory = true
         , ScreenOrientation = ScreenOrientation.Portrait)]
-    public class SplashScreen : MvxSplashScreenActivity<MvxAndroidSetup<Core.App>, Core.App>
+    public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
             : base(Resource.Layout.SplashScreen)
