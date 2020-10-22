77434c4129ffcb21c3ef552b03fc004a366b648f
Andrea Grillo <andrea.grillo@outlook.com>
2020-10-22 23:29:23 +0200

    [Apps] IServiceCollection integration (#194)
    * sperimental code for custom ioc
    
    * [Core] Merge IServiceCollection with Mvx.IoCProvider
    
    * [Core] Remove useless stuff

--- src/CriThink.Client/CriThink.Client.Core/App.cs
+++ src/CriThink.Client/CriThink.Client.Core/App.cs
@@ -1,4 +1,7 @@
-﻿using CriThink.Client.Core.ViewModels;
+﻿using System;
+using CriThink.Client.Core.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using MvvmCross;
 using MvvmCross.IoC;
 using MvvmCross.ViewModels;
 
@@ -10,12 +13,54 @@ namespace CriThink.Client.Core
         {
             base.Initialize();
 
+            InitializeServiceCollection();
+
             CreatableTypes()
                 .EndingWith("Service")
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 
             RegisterAppStart<HomeViewModel>();
+        }
+
+        private static void InitializeServiceCollection()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            MapServiceCollectionToMvx(serviceProvider, serviceCollection);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddHttpClient();
+        }
+
+        private static void MapServiceCollectionToMvx(IServiceProvider serviceProvider,
+            IServiceCollection serviceCollection)
+        {
+            foreach (var serviceDescriptor in serviceCollection)
+            {
+                if (serviceDescriptor.ImplementationType != null)
+                {
+                    Mvx.IoCProvider.RegisterType(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationType);
+                }
+                else if (serviceDescriptor.ImplementationFactory != null)
+                {
+                    var instance = serviceDescriptor.ImplementationFactory(serviceProvider);
+                    Mvx.IoCProvider.RegisterSingleton(serviceDescriptor.ServiceType, instance);
+                }
+                else if (serviceDescriptor.ImplementationInstance != null)
+                {
+                    Mvx.IoCProvider.RegisterSingleton(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationInstance);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unsupported registration type");
+                }
+            }
         }
     }
 }
--- src/CriThink.Client/CriThink.Client.Core/ViewModels/Users/SignUpEmailViewModel.cs
+++ src/CriThink.Client/CriThink.Client.Core/ViewModels/Users/SignUpEmailViewModel.cs
@@ -1,8 +1,16 @@
-﻿using MvvmCross.ViewModels;
+﻿using System;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 
 namespace CriThink.Client.Core.ViewModels.Users
 {
     public class SignUpEmailViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
+
+        public SignUpEmailViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+        }
     }
 }
