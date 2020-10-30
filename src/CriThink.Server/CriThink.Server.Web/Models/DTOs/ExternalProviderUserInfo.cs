62740479f50b5329d7048e8b6f8a22940a48453b
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2020-10-30 20:25:54 +0100

    Create ExternalProviderUserInfo class

---
+++ src/CriThink.Server/CriThink.Server.Web/Models/DTOs/ExternalProviderUserInfo.cs
@@ -0,0 +1,11 @@
+namespace CriThink.Server.Web.Models.DTOs
+{
+    public class ExternalProviderUserInfo
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
