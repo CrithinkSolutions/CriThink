444564858ad109d1c1f9d57bf26bcaf895da56b5
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2020-10-30 20:26:46 +0100

    Create ExternalLoginProviderResolver for resolve DI

---
+++ src/CriThink.Server/CriThink.Server.Web/Delegates/ExternalLoginProviderResolver.cs
@@ -0,0 +1,7 @@
+﻿using CriThink.Common.Endpoints.DTOs.IdentityProvider;
+using CriThink.Server.Web.Interfaces;
+
+namespace CriThink.Server.Web.Delegates
+{
+    public delegate IExternalLoginProvider ExternalLoginProviderResolver(ExternalLoginProvider externalProvider);
+}
