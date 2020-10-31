2b351f60b40bdc2cf021706a88e2e39b5f30e09f
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2020-11-01 00:08:07 +0100

    Add util for UnixTimestamp conversion (by @kipters)

---
+++ src/CriThink.Common/CriThink.Common.Helpers/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CriThink.Common.Helpers
+{
+    public static class DateTimeExtensions
+    {
+        private static DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ToUnixTimestamp(this DateTime self) => (long)(self - Epoch).TotalSeconds;
+
+        public static DateTime AsUnixTimestamp(this long self) => Epoch.AddSeconds(self);
+    }
+}
