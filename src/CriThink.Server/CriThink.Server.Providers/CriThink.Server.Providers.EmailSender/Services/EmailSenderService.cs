14e114459686619b7858a4b5fe1bd24080571d62
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2020-11-08 01:57:39 +0100

    [Web] Send email to SMTP4Dev if IsDevelopment

--- src/CriThink.Server/CriThink.Server.Providers/CriThink.Server.Providers.EmailSender/Services/EmailSenderService.cs
+++ src/CriThink.Server/CriThink.Server.Providers/CriThink.Server.Providers.EmailSender/Services/EmailSenderService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.SimpleEmail;
@@ -8,7 +10,9 @@ using Amazon.SimpleEmail.Model;
 using CriThink.Server.Providers.EmailSender.Settings;
 using CriThink.Server.RazorViews.Services;
 using CriThink.Server.RazorViews.Views.Emails.ConfirmAccount;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 namespace CriThink.Server.Providers.EmailSender.Services
@@ -18,12 +22,14 @@ namespace CriThink.Server.Providers.EmailSender.Services
         private readonly IRazorViewToStringRenderer _razorViewToStringRenderer;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly EmailSettings _emailSettings;
+        private readonly IWebHostEnvironment _environment;
 
-        public EmailSenderService(IRazorViewToStringRenderer razorViewToStringRenderer, IHttpContextAccessor httpContextAccessor, IOptionsSnapshot<EmailSettings> emailSettings)
+        public EmailSenderService(IRazorViewToStringRenderer razorViewToStringRenderer, IHttpContextAccessor httpContextAccessor, IOptionsSnapshot<EmailSettings> emailSettings, IWebHostEnvironment environment)
         {
             _razorViewToStringRenderer = razorViewToStringRenderer ?? throw new ArgumentNullException(nameof(razorViewToStringRenderer));
             _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
             _emailSettings = emailSettings?.Value ?? throw new ArgumentNullException(nameof(emailSettings));
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
         }
 
         public async Task SendAccountConfirmationEmailAsync(string recipient, string userId, string encodedCode)
@@ -59,6 +65,31 @@ namespace CriThink.Server.Providers.EmailSender.Services
         {
             var fromAddress = _emailSettings.FromAddress;
 
+            return _environment.IsDevelopment()
+                ? SendFromLocahost(fromAddress, recipients, subject, htmlBody)
+                : SendFromAWS(fromAddress, recipients, subject, htmlBody);
+        }
+    
+        private static Task SendFromLocahost(string fromAddress, IEnumerable<string> recipients, string subject, string htmlBody)
+        {
+            var smtpClient = new SmtpClient
+            {
+                Host = "localhost",
+                Port = 2525,
+            };
+
+            using var message = new MailMessage(fromAddress, recipients.First())
+            {
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true,
+            };
+
+            return smtpClient.SendMailAsync(message);
+        }
+
+        private static Task SendFromAWS(string fromAddress, IEnumerable<string> recipients, string subject, string htmlBody)
+        {
             using var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.EUCentral1);
             var sendRequest = new SendEmailRequest
             {
