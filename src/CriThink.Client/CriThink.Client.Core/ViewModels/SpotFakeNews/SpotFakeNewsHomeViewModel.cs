e69e8ccb49361195b0e0dd02d47e393fa0b45291
Andrea Grillo <andrea.grillo@outlook.com>
2020-11-11 22:53:31 +0000

    [Droid] Adds bottom bar images

---
+++ src/CriThink.Client/CriThink.Client.Core/ViewModels/SpotFakeNews/SpotFakeNewsHomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CriThink.Client.Core.ViewModels.SpotFakeNews
+{
+    public class SpotFakeNewsHomeViewModel : BaseBottomViewViewModel
+    {
+        public SpotFakeNewsHomeViewModel()
+        {
+            TabId = "spot_fakenews";
+        }
+    }
+}
---
+++ src/CriThink.Client/CriThink.Client.Droid/Views/Games/Fragments/HomeGameView.cs
@@ -0,0 +1,26 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using CriThink.Client.Core.ViewModels;
+using CriThink.Client.Core.ViewModels.Games;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views.Fragments;
+
+// ReSharper disable once CheckNamespace
+namespace CriThink.Client.Droid.Views.Games
+{
+    [MvxFragmentPresentation(typeof(HomeViewModel), null)]
+    [Register(nameof(HomeGameView))]
+    public class HomeGameView : MvxFragment<HomeGameViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = this.BindingInflate(Resource.Layout.homegame_view, null);
+
+            return view;
+        }
+    }
+}
