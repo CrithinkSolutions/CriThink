0ebcc9f5a45c9a5039e4aff74036d661c0d1861d
Andrea Grillo <andrea.grillo@outlook.com>
2020-11-19 23:24:43 +0000

    [Web] Logically deletes service user when an admin is registered

--- src/CriThink.Server/CriThink.Server.Web/Controllers/AdminController.cs
+++ src/CriThink.Server/CriThink.Server.Web/Controllers/AdminController.cs
@@ -2,12 +2,15 @@ using System;
 using System.Threading.Tasks;
 using CriThink.Common.Endpoints;
 using CriThink.Common.Endpoints.DTOs.Admin;
+using CriThink.Server.Core.Exceptions;
 using CriThink.Server.Core.Interfaces;
 using CriThink.Server.Web.ActionFilters;
 using CriThink.Server.Web.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CriThink.Server.Web.Controllers
 {
@@ -23,11 +26,15 @@ namespace CriThink.Server.Web.Controllers
     {
         private readonly IIdentityService _identityService;
         private readonly IDebunkingNewsService _debunkingNewsService;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AdminController> _logger;
 
-        public AdminController(IIdentityService identityService, IDebunkingNewsService debunkingNewsService)
+        public AdminController(IIdentityService identityService, IDebunkingNewsService debunkingNewsService, IConfiguration configuration, ILogger<AdminController> logger)
         {
             _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
             _debunkingNewsService = debunkingNewsService ?? throw new ArgumentNullException(nameof(debunkingNewsService));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _logger = logger;
         }
 
         /// <summary>
@@ -44,6 +51,23 @@ namespace CriThink.Server.Web.Controllers
         public async Task<IActionResult> SignUpAdminAsync([FromBody] AdminSignUpRequest request)
         {
             var creationResponse = await _identityService.CreateNewAdminAsync(request).ConfigureAwait(false);
+
+            var configServiceUserId = _configuration["ServiceUser:Id"];
+            try
+            {
+                var serviceUserId = Guid.Parse(configServiceUserId);
+                var serviceUserRequest = new UserGetRequest
+                {
+                    UserId = serviceUserId
+                };
+
+                await _identityService.SoftDeleteUserAsync(serviceUserRequest).ConfigureAwait(false);
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                _logger?.LogError(ex, "Error while deleting service user", configServiceUserId);
+            }
+
             return Ok(new ApiOkResponse(creationResponse));
         }
 
