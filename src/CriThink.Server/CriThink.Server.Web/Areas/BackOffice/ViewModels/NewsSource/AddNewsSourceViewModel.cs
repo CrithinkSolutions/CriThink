235ae032b6a8a85e4696f9a4fdf1a435723d220a
GabAlpha <gabriele.alfarano@gmail.com>
2021-01-29 17:37:13 +0100

    [Backoffice] Remove source

--- src/CriThink.Server/CriThink.Server.Web/Areas/BackOffice/ViewModels/NewsSource/AddNewsSourceViewModel.cs
+++ src/CriThink.Server/CriThink.Server.Web/Areas/BackOffice/ViewModels/NewsSource/AddNewsSourceViewModel.cs
@@ -5,6 +5,7 @@ namespace CriThink.Server.Web.Areas.BackOffice.ViewModels.NewsSource
     public class AddNewsSourceViewModel
     {
         [Required]
+        [Url]
         public string Uri { get; set; }
         
         [Required]
