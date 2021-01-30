043b4282dbb8843a492698905067188d068f14bb
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2021-01-30 20:02:20 +0100

    [Droid] Updates AppCenter Api Key

--- src/CriThink.Client/CriThink.Client.Droid/appcenter-post-clone.sh
+++ src/CriThink.Client/CriThink.Client.Droid/appcenter-post-clone.sh
@@ -8,3 +8,5 @@ cd $PROJECT_DIR
 
 # Replace base URI
 find $API_URL_FILENAME -type f -exec sed -i '' -e 's/'"$LOCAL_API_URL"'/'"$API_URL"'/' {} \;
+
+find MainApplication.cs -type f -exec sed -i '' -e 's/<APPCENTER_API_KEY>/'"$APPCENTER_API_KEY"'/' {} \;
