7af6b41ce090a2d930e89982eafaf86167828c1f
Andrea Grillo <andrea.grillo@outlook.com>
2021-02-13 17:55:24 +0000

    [Infrastructure] Split entity configurations in different files

---
+++ src/CriThink.Server/CriThink.Server.Infrastructure/Data/EntityConfiguration/DebunkingNewsCountryEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using CriThink.Server.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CriThink.Server.Infrastructure.Data.EntityConfiguration
+{
+    internal class DebunkingNewsCountryEntityConfiguration : IEntityTypeConfiguration<DebunkingNewsCountry>
+    {
+        public void Configure(EntityTypeBuilder<DebunkingNewsCountry> builder)
+        {
+            var countryItaly = new DebunkingNewsCountry
+            {
+                Id = Guid.Parse("575003e3-991c-4ac5-9ce4-3399553f64a7"),
+                Name = "Italy",
+                Code = "it",
+            };
+            var countryUsa = new DebunkingNewsCountry
+            {
+                Id = Guid.Parse("3bd76fc5-5463-4194-b4f9-df111f7c294f"),
+                Name = "United States of America",
+                Code = "us",
+            };
+            var countryUk = new DebunkingNewsCountry
+            {
+                Id = Guid.Parse("812361b1-d1c3-4315-b601-4e060364a1d6"),
+                Name = "United Kingdom",
+                Code = "uk",
+            };
+
+            builder.HasIndex(c => c.Code).IsUnique();
+            builder.HasData((IEnumerable<DebunkingNewsCountry>) new[] { countryItaly, countryUsa, countryUk });
+        }
+    }
+}
---
+++ src/CriThink.Server/CriThink.Server.Infrastructure/Data/EntityConfiguration/IdentityUserLoginEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CriThink.Server.Infrastructure.Data.EntityConfiguration
+{
+    internal class IdentityUserLoginEntityConfiguration : IEntityTypeConfiguration<IdentityUserLogin<Guid>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserLogin<Guid>> builder)
+        {
+            builder.ToTable("aspnet_user_logins");
+        }
+    }
+}
