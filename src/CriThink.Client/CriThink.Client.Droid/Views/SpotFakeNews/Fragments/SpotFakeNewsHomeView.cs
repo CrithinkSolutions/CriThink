4892a46ef5d5051188394df999072bea65997bdc
Gabriele Alfarano <51409681+GabAlpha@users.noreply.github.com>
2021-02-25 00:42:27 +0100

    [Android] Adds HEAD acronym (#501)
    * [Android] Added SVG HEAD view
    
    * [Android] Layout text WIP
    
    * [Android] HEAD View with text
    
    * Fixes
    
    * [Droid] HEAD animation
    
    Co-authored-by: Andrea Grillo <andrea.grillo@outlook.com>

--- src/CriThink.Client/CriThink.Client.Droid/Views/SpotFakeNews/Fragments/SpotFakeNewsHomeView.cs
+++ src/CriThink.Client/CriThink.Client.Droid/Views/SpotFakeNews/Fragments/SpotFakeNewsHomeView.cs
@@ -1,8 +1,12 @@
 ﻿using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Views.Animations;
+using AndroidX.AppCompat.Widget;
+using AndroidX.ConstraintLayout.Widget;
 using CriThink.Client.Core.ViewModels;
 using CriThink.Client.Core.ViewModels.SpotFakeNews;
+using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views.Fragments;
@@ -14,11 +18,42 @@ namespace CriThink.Client.Droid.Views.SpotFakeNews
     [Register(nameof(SpotFakeNewsHomeView))]
     public class SpotFakeNewsHomeView : MvxFragment<SpotFakeNewsHomeViewModel>
     {
+        private static bool IsInitialized;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
             var view = this.BindingInflate(Resource.Layout.spotfakenewshome_view, null);
+
+            var txtHDescription = view.FindViewById<AppCompatTextView>(Resource.Id.txtHDescription);
+            var txtEDescription = view.FindViewById<AppCompatTextView>(Resource.Id.txtEDescription);
+            var txtADescription = view.FindViewById<AppCompatTextView>(Resource.Id.txtADescription);
+            var txtDDescription = view.FindViewById<AppCompatTextView>(Resource.Id.txtDDescription);
+            var layoutH = view.FindViewById<ConstraintLayout>(Resource.Id.layoutH);
+            var layoutE = view.FindViewById<ConstraintLayout>(Resource.Id.layoutE);
+            var layoutA = view.FindViewById<ConstraintLayout>(Resource.Id.layoutA);
+            var layoutD = view.FindViewById<ConstraintLayout>(Resource.Id.layoutD);
+
+            var set = CreateBindingSet();
+
+            set.Bind(txtHDescription).ToLocalizationId("HDescription");
+            set.Bind(txtEDescription).ToLocalizationId("EDescription");
+            set.Bind(txtADescription).ToLocalizationId("ADescription");
+            set.Bind(txtDDescription).ToLocalizationId("DDescription");
+
+            set.Apply();
+
+            if (!IsInitialized)
+            {
+                var bounceAnimation = AnimationUtils.LoadAnimation(Activity, Resource.Animation.bounce_animation);
+                layoutH.StartAnimation(bounceAnimation);
+                layoutE.StartAnimation(bounceAnimation);
+                layoutA.StartAnimation(bounceAnimation);
+                layoutD.StartAnimation(bounceAnimation);
+            }
+
+            IsInitialized = true;
 
             return view;
         }
