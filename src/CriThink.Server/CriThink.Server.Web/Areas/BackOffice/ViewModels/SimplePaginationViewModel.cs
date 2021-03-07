c7d1ac107b8901c0b9a0d7d7c335d42301c3a524
GabAlpha <gabriele.alfarano@gmail.com>
2021-03-07 23:08:38 +0100

    [Backoffice] Set 0 at pageindex

--- src/CriThink.Server/CriThink.Server.Web/Areas/BackOffice/ViewModels/SimplePaginationViewModel.cs
+++ src/CriThink.Server/CriThink.Server.Web/Areas/BackOffice/ViewModels/SimplePaginationViewModel.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace CriThink.Server.Web.Areas.BackOffice.ViewModels
 {
     public class SimplePaginationViewModel
@@ -5,5 +8,18 @@ namespace CriThink.Server.Web.Areas.BackOffice.ViewModels
         public int PageSize { get; set; } = 20;
 
         public int PageIndex { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (PageSize < 1)
+            {
+                yield return new ValidationResult("Page size cannot be less than 1", new[] { nameof(PageSize) });
+            }
+
+            if (PageIndex < 0)
+            {
+                yield return new ValidationResult("Page index cannot be less than 1", new[] { nameof(PageIndex) });
+            }
+        }
     }
 }
