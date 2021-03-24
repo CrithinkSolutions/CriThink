a7ff3cd2bc4430e58902bf82b8c6a98430c3b92f
Andrea Grillo <andrea.grillo@outlook.com>
2021-03-24 23:05:00 +0000

    [Web] Adds Swagger documentation to NewsSourceController

--- src/CriThink.Server/CriThink.Server.Web/Controllers/NewsSourceController.cs
+++ src/CriThink.Server/CriThink.Server.Web/Controllers/NewsSourceController.cs
@@ -5,6 +5,7 @@ using CriThink.Common.Endpoints.DTOs.NewsSource;
 using CriThink.Common.Endpoints.DTOs.UnknownNewsSource;
 using CriThink.Server.Core.Interfaces;
 using CriThink.Server.Web.ActionFilters;
+using CriThink.Server.Web.Models.DTOs;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -21,7 +22,7 @@ namespace CriThink.Server.Web.Controllers
     [ApiValidationFilter]
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    [Route(EndpointConstants.ApiBase + EndpointConstants.NewsSourceBase)] //api/news-source
+    [Route(EndpointConstants.ApiBase + EndpointConstants.NewsSourceBase)]
     public class NewsSourceController : Controller
     {
         private readonly INewsSourceService _newsSourceService;
@@ -36,12 +37,28 @@ namespace CriThink.Server.Web.Controllers
         /// <summary>
         /// Search the given source and returns the list that contains it
         /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET: /api/news-source/search?{newsLink}
+        /// 
+        /// </remarks>
         /// <param name="request">Source to search</param>
-        /// <returns>Returns the list where the source is contained</returns>
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        /// <response code="200">Returns the classification, the description and related
+        /// debunking news</response>
+        /// <response code="400">If the request body is invalid</response>
+        /// <response code="401">If the user is not authorized</response>
+        /// <response code="404">If the given domain is not found</response>
+        /// <response code="500">If the server can't process the request</response>
+        /// <response code="503">If the server is not ready to handle the request</response>
+        [ProducesResponseType(typeof(NewsSourceSearchWithDebunkingNewsResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status503ServiceUnavailable)]
         [Produces("application/json")]
-        [Route(EndpointConstants.NewsSourceSearch)] // api/news-source/search
+        [Route(EndpointConstants.NewsSourceSearch)]
         [HttpGet]
         public async Task<IActionResult> SearchNewsSourceAsync([FromQuery] NewsSourceSearchRequest request)
         {
@@ -50,18 +67,35 @@ namespace CriThink.Server.Web.Controllers
             if (searchResponse is null)
                 return NotFound();
 
-            return Ok(searchResponse);
+            return Ok(new ApiOkResponse(searchResponse));
         }
 
         /// <summary>
-        /// Register the user for being notified if a news source is analyzed
+        /// Register the user for being notified when a news source is analyzed
         /// </summary>
-        /// <param name="request">User info</param>
-        /// <returns>Returns the list where the source is contained</returns>
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST: /api/news-source/register-for-notification
+        ///     {
+        ///         "email": "email",
+        ///         "uri": "uri",
+        ///     }
+        ///         
+        /// </remarks>
+        /// <param name="request">Email and the unknown uri</param>
+        /// <response code="204">Returns when operation succeeds</response>
+        /// <response code="401">If the user is not authorized</response>
+        /// <response code="404">If the given domain is not found</response>
+        /// <response code="500">If the server can't process the request</response>
+        /// <response code="503">If the server is not ready to handle the request</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status503ServiceUnavailable)]
         [Produces("application/json")]
-        [Route(EndpointConstants.NewsSourceRegisterForNotification)] // api/news-source/register-for-notification
+        [Route(EndpointConstants.NewsSourceRegisterForNotification)]
         [HttpPost]
         public async Task<IActionResult> RequestNotificationForUnknownSourceAsync([FromBody] NewsSourceNotificationForUnknownDomainRequest request)
         {
