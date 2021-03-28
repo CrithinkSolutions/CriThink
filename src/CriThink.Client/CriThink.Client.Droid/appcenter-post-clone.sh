520804be5d2a7026d9b63d413f88263ebc5d36cb
invertostringhe <m.tammaccaro@outlook.com>
2021-03-28 23:01:36 +0200

    [Pipeline] Moves to Constants directory before updating DeepLinkConstants.cs

--- src/CriThink.Client/CriThink.Client.Droid/appcenter-post-clone.sh
+++ src/CriThink.Client/CriThink.Client.Droid/appcenter-post-clone.sh
@@ -11,4 +11,7 @@ find $API_URL_FILENAME -type f -exec sed -i '' -e 's/'"$LOCAL_API_URL"'/'"$API_U
 
 find MainApplication.cs -type f -exec sed -i '' -e 's/<APPCENTER_API_KEY>/'"$APPCENTER_API_KEY"'/' {} \;
 
+cd Constants/
+
 find DeepLinkConstants.cs -type f -exec sed -i '' -e 's/crithinkdemo.com/'"$SCHEMA_HOST"'/' {} \;
+
