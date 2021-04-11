e5fe0f7c26d5040fa6d9ab0019891f46c1c9468c
Andrea Grillo <andrea.grillo@outlook.com>
2021-04-11 23:19:44 +0100

    misc

--- src/CriThink.Server/CriThink.Server.Web/Startup.cs
+++ src/CriThink.Server/CriThink.Server.Web/Startup.cs
@@ -181,7 +181,7 @@ namespace CriThink.Server.Web
         {
             services.AddDbContext<CriThinkDbContext>(options =>
             {
-                var connectionString = Configuration.GetConnectionString("CriThinkDbPgSqlConnection");
+                var connectionString = Configuration.GetConnectionString("CriThinkDbPgSqlConnection") ?? "";
                 options.UseNpgsql(connectionString, npgsqlOptionsAction: sqlOptions =>
                 {
                     sqlOptions.EnableRetryOnFailure(
