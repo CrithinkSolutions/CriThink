317e9f5121515edd50dfcad2a52e3e23aabc6aea
Andrea Grillo <andrea.grillo@outlook.com>
2021-04-12 12:47:25 +0100

    print both

--- src/CriThink.Server/CriThink.Server.Infrastructure/Data/CriThinkDbContext.cs
+++ src/CriThink.Server/CriThink.Server.Infrastructure/Data/CriThinkDbContext.cs
@@ -65,16 +65,16 @@ namespace CriThink.Server.Infrastructure.Data
             if (args?.Any() == true)
             {
                 var cs = args[0];
-                Console.WriteLine($"------------------- {cs} -------------------");
+                Console.WriteLine($"------------------- cs: {cs} -------------------");
                 optionsBuilder.UseNpgsql(cs).UseSnakeCaseNamingConvention(System.Globalization.CultureInfo.InvariantCulture);
             }
 
-            //var env = Environment.GetEnvironmentVariable("STAGING_CRITHINK_SERVER_CONNECTIONSTRINGS_CRITHINKDBPGSQLCONNECTION");
-            //if (!string.IsNullOrWhiteSpace(env))
-            //{
-            //    Console.WriteLine($"------------------- {env} -------------------");
-            //    optionsBuilder.UseNpgsql(env).UseSnakeCaseNamingConvention(System.Globalization.CultureInfo.InvariantCulture);
-            //}
+            var env = Environment.GetEnvironmentVariable("STAGING_CRITHINK_SERVER_CONNECTIONSTRINGS_CRITHINKDBPGSQLCONNECTION");
+            if (!string.IsNullOrWhiteSpace(env))
+            {
+                Console.WriteLine($"------------------- env: {env} -------------------");
+                optionsBuilder.UseNpgsql(env).UseSnakeCaseNamingConvention(System.Globalization.CultureInfo.InvariantCulture);
+            }
 
             //optionsBuilder.UseNpgsql(env).UseSnakeCaseNamingConvention(System.Globalization.CultureInfo.InvariantCulture); ;
 
