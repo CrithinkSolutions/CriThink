90001d14123297294ddf2220d894e4fce3f8e177
westleyrotolo <westley.rotolo@altran.it>
2021-09-30 00:55:43 +0200

    implement new ui for sign up page

--- src/CriThink.Client/CriThink.Client.Droid/Views/Users/Activities/SignUpEmailView.cs
+++ src/CriThink.Client/CriThink.Client.Droid/Views/Users/Activities/SignUpEmailView.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
+using Android.Widget;
 using AndroidX.AppCompat.Widget;
+using AndroidX.Core.Content;
 using CriThink.Client.Core.ViewModels.Users;
 using CriThink.Client.Droid.Constants;
 using CriThink.Client.Droid.Controls;
@@ -13,6 +16,7 @@ using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Binding;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
+using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
 
 // ReSharper disable once CheckNamespace
 namespace CriThink.Client.Droid.Views.Users
@@ -20,12 +24,12 @@ namespace CriThink.Client.Droid.Views.Users
     [IntentFilter(
         actions: new[] { Android.Content.Intent.ActionView },
         Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
-        DataSchemes = new[] { DeepLinkConstants.SchemaHTTP, DeepLinkConstants.SchemaHTTPS },
+        DataSchemes = new[] { DeepLinkConstants.SchemaHTTP, DeepLinkConstants.SchemaHTTPS },    
         DataHost = DeepLinkConstants.SchemaHost,
         DataPathPrefix = "/" + DeepLinkConstants.SchemaPrefixEmailConfirmation,
         AutoVerify = false)]
     [MvxActivityPresentation]
-    [Activity(LaunchMode = LaunchMode.SingleTop, ClearTaskOnLaunch = true)]
+    [Activity(LaunchMode = LaunchMode.SingleTop, ClearTaskOnLaunch = true, Theme = "@style/AppTheme.LightArrow")]
     public class SignUpEmailView : MvxActivity<SignUpEmailViewModel>
     {
         protected override void OnCreate(Bundle bundle)
@@ -39,7 +43,7 @@ namespace CriThink.Client.Droid.Views.Users
             SupportActionBar.SetHomeButtonEnabled(true);
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
             SupportActionBar.SetDisplayOptions((int) ActionBarDisplayOptions.ShowCustom, (int) ActionBarDisplayOptions.ShowCustom);
-
+            Window.SetStatusBarColor(new Color(ContextCompat.GetColor(this, Resource.Color.accent)));
             var txtTitle = FindViewById<AppCompatTextView>(Resource.Id.txtTitle);
             var txtCaption = FindViewById<AppCompatTextView>(Resource.Id.txtCaption);
             var email = FindViewById<TextInputEditText>(Resource.Id.txtInput_email);
@@ -49,9 +53,13 @@ namespace CriThink.Client.Droid.Views.Users
             var btnSignUp = FindViewById<AppCompatButton>(Resource.Id.btnSignUp);
             var loader = FindViewById<LoaderView>(Resource.Id.loader);
             var txtEditEmail = FindViewById<TextInputLayout>(Resource.Id.txtEditEmail);
+            var tvHeaderEmail = FindViewById<TextView>(Resource.Id.tv_header_email);
             var txtEditUsername = FindViewById<TextInputLayout>(Resource.Id.txtEditUsername);
+            var tvHeaderUsername = FindViewById<TextView>(Resource.Id.tv_header_username);
             var txtEditPassword = FindViewById<TextInputLayout>(Resource.Id.txtEditPassword);
+            var tvHeaderPassord = FindViewById<TextView>(Resource.Id.tv_header_password);
             var txtEditRepeatPassword = FindViewById<TextInputLayout>(Resource.Id.txtEditRepeatPassword);
+            var tvHeaderRepeatPassword = FindViewById<TextView>(Resource.Id.tv_header_repeat_password);
             var imgAvatar = FindViewById<MvxSvgCachedImageView>(Resource.Id.imgAvatar);
             var imgEditAvatar = FindViewById<MvxCachedImageView>(Resource.Id.imgEditAvatar);
 
@@ -76,10 +84,10 @@ namespace CriThink.Client.Droid.Views.Users
             set.Bind(imgAvatar).For(v => v.ImagePath).To(vm => vm.CustomImagePath);
 
             set.Bind(btnSignUp).For(v => v.Text).ToLocalizationId("SignUp");
-            set.Bind(txtEditEmail).For(v => v.Hint).ToLocalizationId("EmailHint");
-            set.Bind(txtEditUsername).For(v => v.Hint).ToLocalizationId("UsernameHint");
-            set.Bind(txtEditPassword).For(v => v.Hint).ToLocalizationId("PasswordHint");
-            set.Bind(txtEditRepeatPassword).For(v => v.Hint).ToLocalizationId("RepeatPasswordHint");
+            set.Bind(tvHeaderEmail).For(v => v.Text).ToLocalizationId("EmailHint");
+            set.Bind(tvHeaderUsername).For(v => v.Text).ToLocalizationId("UsernameHint");
+            set.Bind(tvHeaderPassord).For(v => v.Text).ToLocalizationId("PasswordHint");
+            set.Bind(tvHeaderRepeatPassword).For(v => v.Text).ToLocalizationId("RepeatPasswordHint");
             set.Bind(txtTitle).ToLocalizationId("SignUpEmailTitle");
             set.Bind(txtCaption).ToLocalizationId("SignUpEmailCaption");
 
