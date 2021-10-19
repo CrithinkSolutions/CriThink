a3f4e6a1918c24e6c4bc15150c3fedab76f3b4b7
Andrea Grillo <andrea.grillo@outlook.com>
2021-10-19 22:40:48 +0200

    [Droid] Made with love text

--- src/CriThink.Client/CriThink.Client.Droid/Views/Users/Fragments/AboutView.cs
+++ src/CriThink.Client/CriThink.Client.Droid/Views/Users/Fragments/AboutView.cs
@@ -1,5 +1,7 @@
 ﻿using Android.OS;
 using Android.Runtime;
+using Android.Text;
+using Android.Text.Style;
 using Android.Views;
 using AndroidX.AppCompat.Widget;
 using AndroidX.RecyclerView.Widget;
@@ -11,6 +13,7 @@ using MvvmCross.DroidX.RecyclerView;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views.Fragments;
+using static Android.Widget.TextView;
 
 // ReSharper disable once CheckNamespace
 namespace CriThink.Client.Droid.Views.Users
@@ -29,12 +32,29 @@ namespace CriThink.Client.Droid.Views.Users
             var imgProfile = view.FindViewById<MvxSvgCachedImageView>(Resource.Id.imgProfile);
             var btnViewProfile = view.FindViewById<AppCompatButton>(Resource.Id.btnViewProfile);
             var recyclerOptions = view.FindViewById<MvxRecyclerView>(Resource.Id.recyclerOptions);
+            var txtMadeWithLove = view.FindViewById<AppCompatTextView>(Resource.Id.txtMadeWithLove);
+            var txtMadeWithLoveTwo = view.FindViewById<AppCompatTextView>(Resource.Id.txtMadeWithLoveTwo);
 
             var layoutManager = new LinearLayoutManager(Activity) { AutoMeasureEnabled = true };
             recyclerOptions.SetLayoutManager(layoutManager);
             recyclerOptions.HasFixedSize = true;
 
             recyclerOptions.ItemTemplateSelector = new MenuItemSelector();
+
+            var txtMadeWith = "Made with";
+            var hearth = " ❤️ ";
+            var last = "in Italy and United Kingdom";
+
+            SpannableStringBuilder builderFirstRow = new SpannableStringBuilder();
+            SpannableString spannableMadeWith = new SpannableString(txtMadeWith);
+            builderFirstRow.Append(spannableMadeWith);
+
+            SpannableString str2 = new SpannableString(hearth);
+            str2.SetSpan(new ForegroundColorSpan(Android.Graphics.Color.Red), 0, str2.Length(), 0);
+            builderFirstRow.Append(str2);
+
+            txtMadeWithLove.SetText(builderFirstRow, BufferType.Spannable);
+            txtMadeWithLoveTwo.SetText(last, BufferType.Normal);
 
             var set = CreateBindingSet();
 
