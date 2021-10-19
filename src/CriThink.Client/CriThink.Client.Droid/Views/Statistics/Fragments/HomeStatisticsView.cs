86681c8733dd9013259dd602b4d7496130033bde
Westley Rotolo <westley.rotolo@altran.it>
2021-10-19 23:26:10 +0200

    implement view

--- src/CriThink.Client/CriThink.Client.Droid/Views/Statistics/Fragments/HomeStatisticsView.cs
+++ src/CriThink.Client/CriThink.Client.Droid/Views/Statistics/Fragments/HomeStatisticsView.cs
@@ -1,8 +1,12 @@
-﻿using Android.OS;
+﻿using System;
+using Android.Animation;
+using Android.Graphics;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using AndroidX.AppCompat.Widget;
 using AndroidX.ConstraintLayout.Widget;
+using AndroidX.Core.Content;
 using CriThink.Client.Core.ViewModels;
 using CriThink.Client.Core.ViewModels.Statistics;
 using MvvmCross.Binding.BindingContext;
@@ -11,6 +15,9 @@ using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views.Fragments;
 using MvvmCross.Plugin.Visibility;
+using MvvmCross.WeakSubscription;
+using static Android.Animation.ValueAnimator;
+using static AndroidHUD.Resource;
 
 // ReSharper disable once CheckNamespace
 namespace CriThink.Client.Droid.Views.Statistics
@@ -19,35 +26,35 @@ namespace CriThink.Client.Droid.Views.Statistics
     [Register(nameof(HomeStatisticsView))]
     public class HomeStatisticsView : MvxFragment<HomeStatisticsViewModel>
     {
+        private AppCompatTextView _txtActiveUsers;
+        private AppCompatTextView _txtTotalSearches;
+        private AppCompatTextView _txtUserSearches;
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
             var view = this.BindingInflate(Resource.Layout.homestatistics_view, null);
 
-            var txtMottoOne = view.FindViewById<AppCompatTextView>(Resource.Id.txtMottoOne);
-            var txtMottoTwo = view.FindViewById<AppCompatTextView>(Resource.Id.txtMottoTwo);
             var txtActiveUsersHeader = view.FindViewById<AppCompatTextView>(Resource.Id.txtActiveUsersHeader);
-            var txtActiveUsers = view.FindViewById<AppCompatTextView>(Resource.Id.txtActiveUsers);
+            _txtActiveUsers = view.FindViewById<AppCompatTextView>(Resource.Id.txtActiveUsers);
             var txtTotalSearchesHeader = view.FindViewById<AppCompatTextView>(Resource.Id.txtTotalSearchesHeader);
-            var txtTotalSearches = view.FindViewById<AppCompatTextView>(Resource.Id.txtTotalSearches);
+            _txtTotalSearches = view.FindViewById<AppCompatTextView>(Resource.Id.txtTotalSearches);
             var txtUserSearchesHeader = view.FindViewById<AppCompatTextView>(Resource.Id.txtUserSearchesHeader);
-            var txtUserSearches = view.FindViewById<AppCompatTextView>(Resource.Id.txtUserSearches);
+            _txtUserSearches = view.FindViewById<AppCompatTextView>(Resource.Id.txtUserSearches);
             var viewContent = view.FindViewById<ConstraintLayout>(Resource.Id.viewContent);
             var viewError = view.FindViewById<ConstraintLayout>(Resource.Id.viewError);
             var txtError = view.FindViewById<AppCompatTextView>(Resource.Id.txtError);
 
-            var set = CreateBindingSet();
 
-            set.Bind(txtActiveUsers).To(vm => vm.PlatformUsers);
-            set.Bind(txtTotalSearches).To(vm => vm.PlatformSearches);
-            set.Bind(txtUserSearches).To(vm => vm.UserSearches);
+            ViewModel.WeakSubscribe(() => ViewModel.PlatformUsers, (sender, e) => CountAnimation(_txtActiveUsers, ViewModel.PlatformUsers));
+            ViewModel.WeakSubscribe(() => ViewModel.PlatformSearches, (sender, e) => CountAnimation(_txtTotalSearches, ViewModel.PlatformSearches));
+            ViewModel.WeakSubscribe(() => ViewModel.UserSearches, (sender, e) => CountAnimation(_txtUserSearches, ViewModel.UserSearches));
+
+            var set = CreateBindingSet();
 
             set.Bind(viewContent).For(v => v.Visibility).To(vm => vm.HasFailed).WithConversion<MvxInvertedVisibilityValueConverter>();
             set.Bind(viewError).For(v => v.BindVisible()).To(vm => vm.HasFailed);
 
-            set.Bind(txtMottoOne).ToLocalizationId("MottoOne");
-            set.Bind(txtMottoTwo).ToLocalizationId("MottoTwo");
             set.Bind(txtActiveUsersHeader).ToLocalizationId("ActiveUsers");
             set.Bind(txtTotalSearchesHeader).ToLocalizationId("TotalSearches");
             set.Bind(txtUserSearchesHeader).ToLocalizationId("UserSearches");
@@ -55,7 +62,38 @@ namespace CriThink.Client.Droid.Views.Statistics
 
             set.Apply();
 
+
+
             return view;
         }
+        public override void OnStart()
+        {
+            base.OnStart();
+            CountAnimation(_txtActiveUsers, ViewModel.PlatformUsers);
+            CountAnimation(_txtTotalSearches, ViewModel.PlatformSearches);
+            CountAnimation(_txtUserSearches, ViewModel.UserSearches);
+        }
+        void CountAnimation(AppCompatTextView textView, long value)
+        {
+            var animator = new ValueAnimator();
+            animator.SetObjectValues(0, (int)value); 
+            animator.AddUpdateListener(new CountAnimatorUpdateListener(textView));
+            animator.SetDuration(Math.Min(5000, Math.Max(value / 100, 1000)));
+            animator.Start();
+        }
     }
+    public class CountAnimatorUpdateListener : Java.Lang.Object, IAnimatorUpdateListener
+    {
+        private readonly AppCompatTextView _textView;
+        public CountAnimatorUpdateListener(AppCompatTextView textView)
+        {
+            _textView = textView;
+        }
+
+        public void OnAnimationUpdate(ValueAnimator animation)
+        {
+            _textView.Text = $"{(int)animation.AnimatedValue:000,000}";
+        }
+    }
+
 }
